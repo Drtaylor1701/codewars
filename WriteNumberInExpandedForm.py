ccacc15f5d0e881662f457a3db18cd43ec046316
drtaylor1701 <trekker35@gmail.com>
2018-07-09 06:52:06 -0700

    Set up number to convert to string

---
+++ WriteNumberInExpandedForm.py
@@ -0,0 +1,8 @@
+def expandedForm(num):
+    #convert num to string
+    num = str(num)
+    print(num)
+    #get length of string
+    #figure out place value for each item
+    #add in the operation signs
+    #retun completed string
