f88a0d6e7117e154f9bde9e11d8ad90a1d6f6255
drtaylor1701 <trekker35@gmail.com>
2018-07-19 12:48:04 -0700

    first attempt at actually getting the math right.

--- NumberOfPeopleInTheBus.py
+++ NumberOfPeopleInTheBus.py
@@ -1,4 +1,16 @@
 def number(bus_stops):
+    people = 0
     # Good Luck!
-    for array in arrays:
+    for array in bus_stops:
         print(array)
+        index = 0
+        for number in array:
+            print(number)
+            if index == 0:
+                people = people + number
+            else:
+                people = people - number
+            print(people)
+            
+bus_stops = [[10,0],[3,5],[5,8]]
+number(bus_stops)
