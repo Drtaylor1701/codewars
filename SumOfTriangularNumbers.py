987c1f0fb1f9809735f428b51c8a6210c1e87948
drtaylor1701 <trekker35@gmail.com>
2018-07-24 07:36:15 -0700

    Inserting preformatted code from website
    https://www.codewars.com/kata/580878d5d27b84b64c000b51/train/python

---
+++ SumOfTriangularNumbers.py
@@ -0,0 +1,7 @@
+"""Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+
+Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc.""""
+
+def sum_triangular_numbers(n):
+    #your code here
+    return sum_
