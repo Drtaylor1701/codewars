14a900c3ff1a1da34b525fd89ec92f032e8d072e
drtaylor1701 <trekker35@gmail.com>
2018-08-11 13:55:47 -0700

    started aresimilar

--- addborder.py
+++ addborder.py
@@ -1,11 +1,9 @@
 def addBorder(picture):
     index = 0
-    topBottom = len(picture) / 2
-    topBottom = int(topBottom)
-    top = "**" * topBottom + "**"
-    newArray = [top]
+    topBottom = "*" * len(picture[0]) + "**"
+    newArray = [topBottom]
     for element in picture:
         newArray.append("*" + element + "*")
 
-    newArray.append(top)
+    newArray.append(topBottom)
     return newArray
